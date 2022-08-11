da3386befcab42ac44308fb7b40bdbb989ddcbec
Thiago Matias Martins <41652032+mmt93@users.noreply.github.com>
2022-08-11 12:19:34 -0300

    Update dockerd-entrypoint.sh

--- dockerd-entrypoint.sh
+++ dockerd-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [[ "$1" = "serve" ]]; then
     curl -o model-store/qna2.mar "${URL_QNA2}"
     shift 1
-    torchserve --start --ts-config /home/model-server/config.properties
+    torchserve --start --model-store model-store
 else
     eval "$@"
 fi
